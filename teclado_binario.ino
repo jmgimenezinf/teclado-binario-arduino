0400464a5455b130fc9e399e105f1ae4df127c0c
Juan Martin Gimenez <jm.gimenez.inf@gmail.com>
2017-03-10 01:22:37 -0300

    Listo para presentar

--- teclado_binario.ino
+++ teclado_binario.ino
@@ -2,10 +2,9 @@
 int tecla_cero;
 int tecla_uno;
 int j=0;
-int binario_leido[8];
+unsigned char num = 0;
 
 void setup() {
-  // put your setup code here, to run once:
   pinMode(4,INPUT); // PULL DOWN 1
   pinMode(2,INPUT);//  PULL UP 0
   Serial.begin(9600);
@@ -14,39 +13,40 @@ void setup() {
 
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-  tecla_uno=digitalRead(4);
-  tecla_cero=digitalRead(2);
+  tecla_uno = digitalRead(4);
+  tecla_cero = digitalRead(2);
   delay(250);
 
+  if ((tecla_cero == LOW) or (tecla_uno == HIGH)) {
+      Serial.print("bit ");
+      Serial.print(j+1, DEC);
+      
+      if(tecla_cero == LOW){
+        Serial.print(": 0");
+      }
+      
+      if (tecla_uno == HIGH){
+        num = num | (1 << (7-j));
+        Serial.print(": 1");
+      }
 
-  if ((tecla_cero == LOW)or (tecla_uno==HIGH)) {
-        Serial.println(j);
-      if(tecla_cero==LOW){
-         binario_leido[j]=0;
-        Serial.println("pulsaste la tecla cero");   
-      }
-      if (tecla_uno==HIGH){
-        binario_leido[j]=1;
-        Serial.println("pulsaste la tecla uno");
-      }
-      if(j==7){
+      Serial.println("");
+      j++;
+      
+      if (j==8) {
         j=0;
-         float num = 0;
-            int tempNUM = 0;
-            int i;
-            for(i = 0; i < 7; i++){
-                num = (pow(2, i) * binario_leido[i]) + num;
-                Serial.print(num);
-            }
-            for(i = 1; i < 7; i++){
-                tempNUM = num - (pow(10,i) * int(num/pow(10,i))) - tempNUM;
-                Serial.print(byte(tempNUM/pow(10,i-1)), 'DEC');
-            }
-      }else{
-       j=j+1;}
-      Serial.println(j);
+        Serial.print("El caracter ingresado es: ");
+        Serial.println(num);
+        Serial.print("El binario ingresado es: ");
+        if(num & (1 << 7) == 0) {
+          Serial.print("0");
+        }
+        Serial.println(num, BIN);
+        Serial.print("El numero ingresado es: ");
+        Serial.println(num, DEC);
+        num = 0;
+      }
       delay(250);
    }
 }
+
